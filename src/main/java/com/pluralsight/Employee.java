4b3a15cef30496451ae0148caeee78f79c059ccb
Tjones <taouris.jones@gmail.com>
2025-10-07 14:52:05 -0400

    commit

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -2,63 +2,51 @@ package com.pluralsight;
 
 public class Employee {
 
-    //id|name|hours-worked|pay-rate
-    private int employeeid;
-    private String name;
-    private String hoursWorked;
-    private String payRate;
+        private int employeeId;
+        private String name;
+        private double hoursWorked, payRate;
 
-    //  10|Dana Wyatt|52.5|12.50
-    //  20|Ezra Aiden|17|16.75
-    //  30|Brittany Sings|40|16.50
-    //   40|Zephaniah Hughes|2|10.0
-    public Employee(int id, String name, String hoursWorked, String payRate) {
-        this.employeeid = employeeid;
-        this.name = name;
-        this.hoursWorked = hoursWorked;
-        this.payRate = payRate;
+
+        public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+            this.employeeId = employeeId;
+            this.name = name;
+            this.hoursWorked = hoursWorked;
+            this.payRate = payRate;
+        }
+
+        public double getGrossPay() {
+            return this.hoursWorked * this.payRate;
+        }
+
+        public int getEmployeeId() {
+            return employeeId;
+        }
+
+        public void setEmployeeId(int employeeId) {
+            this.employeeId = employeeId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getHoursWorked() {
+            return hoursWorked;
+        }
+
+        public void setHoursWorked(double hoursWorked) {
+            this.hoursWorked = hoursWorked;
+        }
+
+        public double getPayRate() {
+            return payRate;
+        }
+
+        public void setPayRate(double payRate) {
+            this.payRate = payRate;
+        }
     }
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + employeeid +
-                ", name='" + name + '\'' +
-                ", hoursWorked='" + hoursWorked + '\'' +
-                ", payRate='" + payRate + '\'' +
-                '}';
-    }
-    public int getemployeeId() {
-        return employeeid;
-    }
-
-    public void setId(int employeeid) {
-        this.employeeid = employeeid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getHoursWorked() {
-        return hoursWorked;
-    }
-
-    public void setHoursWorked(String hoursWorked) {
-        this.hoursWorked = hoursWorked;
-    }
-
-    public String getPayRate() {
-        return payRate;
-    }
-
-    public void setPayRate(String payRate) {
-        this.payRate = payRate;
-    }
-
-
-
-}
--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,37 +1,67 @@
 package com.pluralsight;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
+        System.out.println(":Enter Employee id");
+        int id = userInput.nextInt();
 
-      //  10|Dana Wyatt|52.5|12.50
-      //  20|Ezra Aiden|17|16.75
-      //  30|Brittany Sings|40|16.50
-     //   40|Zephaniah Hughes|2|10.0
+        System.out.println("Enter Employee Name:");
+        String name = userInput.next();
+
+        System.out.println("Enter Hours Worked:");
+        double hoursWorked = userInput.nextDouble();
+
+        System.out.println("Enter Pay Rate:");
+        double payRate = userInput.nextDouble();
 
         try {
             FileReader fileReader = new FileReader("src/main/resources/employees.csv");
-            BufferedReader bufReader = new BufferedReader(fileReader);
+            BufferedWriter writer = new BufferedWriter(fileReader);
+
+//            bufReader.readLine(); //Alternative method to skip first line of csv file
 
             String input;
 
-            while((input = bufReader.readLine()) != null) {
-               String[] scrolls = input.split("\\|");
+            Employee[] employeeList = new Employee[10];
 
-                System.out.println(scrolls[0]);
-                System.out.println(scrolls[1]);
-                System.out.println(scrolls[2]);
-                System.out.println(scrolls[3]);
+            int count = 0;
+
+
+            while((input = bufReader.readLine()) != null) {
+                String[] tokens = input.split("\\|");
+                if (!tokens[0].equals("id")) { //Skipping the header row
+
+                    int id = Integer.parseInt(tokens[0]); //id
+                    String name = tokens[1];
+                    double hoursWorked = Double.parseDouble(tokens[2]);
+                    double payRate = Double.parseDouble(tokens[3]);
+
+                    employeeList[count] = new Employee(id, name, hoursWorked, payRate);
+
+                   String employeeInfo = String.format(("EmployeeID: %d\nEmployee Name: %s\nGross Pay: $%.2f\n\n", employeeList[count].getEmployeeId(), employeeList[count].getName(), employeeList[count].getGrossPay());
+
+
+                    count++;
+                }
+
             }
 
-            bufReader.close();
+            System.out.println(employeeList[0].getName());
+
+            bufWriter.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-
-
     }
+
 }
+
+
